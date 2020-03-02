9e310139263f8b0f71d85e4138b49aa80a8e3827
Andre Litty <andre.litty@gmx.de>
2020-03-02 13:45:53 +0700

    Added commands to set local ip address, mtu and hostname, implemented command to generically invoke subprocess run

--- gw_cli.py
+++ gw_cli.py
@@ -1,5 +1,52 @@
 import click
+import subprocess
+
+
+class InvalidArgumentException(Exception):
+    pass
+
+def run_subprocess(args=[]):
+    if not args or len(args) == 0:
+        raise InvalidArgumentException()
+    try:
+        result = subprocess.call(
+            args,
+            capture_output=True,
+            check=True
+            )
+    except subprocess.CalledProcessError as cpe:
+        result = cpe
+    except Exception:
+        result = None
+    return result
 
 @click.Command()
-def cli():
-    pass
+@click.option('--address', help='IPv4 address to assign to device')
+@click.option('--netmask', help='IPv4 netmask')
+@click.option('--device', help='Device to assign the address to')
+def set_ipv4(address, netmask, device):
+    try:
+        args = ['ip', 'addr', 'add' address, netmask, 'dev', device]
+        result = run_subprocess(args=args)
+    except InvalidArgumentException:
+        pass
+
+@click.Command()
+@click.option('--mtu', help='MTU to assign to device')
+@click.option('--device', help='Device to assign the MTU to')
+def set_mtu(mtu, device):
+    try:
+        args = ['ip', 'link', 'set' device, 'mtu', mtu]
+        result = run_subprocess(args=args)
+    except InvalidArgumentException:
+        pass
+
+@click.Command()
+@click.option('--hostname', help='New hostname')
+def set_hostname(hostname):
+    try:
+        args = ['hostnamectl', 'set-hostname', hostname]
+        result = run_subprocess(args=args)
+    except InvalidArgumentException:
+        pass
+
