19300e8d77dfef5a00cbd98e9b5435365d74ef26
Andre Litty <andre.litty@gmx.de>
2020-03-03 00:47:24 +0700

    Enhanced test cases using unittest module and writing further test cases

--- gw_cli.py
+++ gw_cli.py
@@ -2,12 +2,25 @@ import click
 import subprocess
 
 
+class EmptyArgsException(Exception):
+    
+    def __init__(self, message='The passed arguments were empty'):
+        self.message = message
+    
+    def __str__(self):
+        return self.message
+
 class InvalidArgumentException(Exception):
-    pass
+    
+    def __init__(self, message='The passed argument wasn\'t valid'):
+        self.message = message
+    
+    def __str__(self):
+        return self.message
 
 def run_subprocess(args=[]):
     if not args or len(args) == 0:
-        raise InvalidArgumentException()
+        raise EmptyArgsException()
     try:
         result = subprocess.run(
             args,
@@ -25,28 +38,25 @@ def run_subprocess(args=[]):
 @click.option('--netmask', help='IPv4 netmask')
 @click.option('--device', help='Device to assign the address to')
 def set_ipv4(address, netmask, device):
-    try:
-        args = ['ip', 'addr', 'add', address, netmask, 'dev', device]
-        result = run_subprocess(args=args)
-    except InvalidArgumentException:
-        pass
+    if len(address) == 0 or len(netmask) == 0 or len(device) == 0:
+        raise InvalidArgumentException
+    args = ['ip', 'addr', 'add', address, netmask, 'dev', device]
+    result = run_subprocess(args=args)
 
 @click.command()
 @click.option('--mtu', help='MTU to assign to device')
 @click.option('--device', help='Device to assign the MTU to')
 def set_mtu(mtu, device):
-    try:
-        args = ['ip', 'link', 'set', device, 'mtu', mtu]
-        result = run_subprocess(args=args)
-    except InvalidArgumentException:
-        pass
+    if len(mtu) == 0 or len(device) == 0:
+        raise InvalidArgumentException
+    args = ['ip', 'link', 'set', device, 'mtu', mtu]
+    result = run_subprocess(args=args)
 
 @click.command()
 @click.option('--hostname', help='New hostname')
 def set_hostname(hostname):
-    try:
-        args = ['hostnamectl', 'set-hostname', hostname]
-        result = run_subprocess(args=args)
-    except InvalidArgumentException:
-        pass
+    if len(hostname) == 0:
+        raise InvalidArgumentException
+    args = ['hostnamectl', 'set-hostname', hostname]
+    result = run_subprocess(args=args)
 
--- test_gw_cli.py
+++ test_gw_cli.py
@@ -1,41 +1,169 @@
 from click.testing import CliRunner
-from gw_cli import run_subprocess, set_hostname, set_ipv4, set_mtu
+from gw_cli import run_subprocess, set_hostname, set_ipv4, set_mtu, InvalidArgumentException
 
-import inspect
+import unittest
 
-
-class GwCliTest(object):
-
+class TestGwCli(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.runner = CliRunner()
+    
     def test_run_subprocess_success(self):
         result = run_subprocess(args=['ls', '-la'])
-        assert result.returncode == 0
+        self.assertEqual(result.returncode, 0)
 
     def test_run_subprocess_failure(self):
         result = run_subprocess(args=['ls', '-y'])
-        assert result.returncode != 0
+        self.assertNotEqual(result.returncode, 0)
 
     def test_set_hostname_success(self):
-        pass
+        result = self.runner.invoke(set_hostname, args=[
+            '--hostname',
+            'new_hostname'
+            ])
+        self.assertEqual(result.returncode, 0)
 
-    def test_set_hostname_failure(self):
-        pass
+    def test_set_hostname_failure_no_hostname(self):
+        result = self.runner.invoke(set_hostname, args=[
+            '--hostname'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, SystemExit)
+
+    def test_set_hostname_failure_empty_hostname(self):
+        result = self.runner.invoke(set_hostname, args=[
+            '--hostname',
+            ''
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, InvalidArgumentException)
 
     def test_set_ipv4_success(self):
-        pass
+        result = self.runner.invoke(set_ipv4, args=[
+            '--address',
+            '127.0.0.2',
+            '--netmask',
+            '24',
+            '--device',
+            'etn0'
+            ])
+        self.assertEqual(result.returncode, 0)
 
-    def test_set_ipv4_failure(self):
-        pass
+    def test_set_ipv4_failure_no_address(self):
+        result = self.runner.invoke(set_ipv4, args=[
+            '--netmask',
+            '24',
+            '--device',
+            'etn0'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, SystemExit)
+
+    def test_set_ipv4_failure_empty_address(self):
+        result = self.runner.invoke(set_ipv4, args=[
+            '--address',
+            '',
+            '--netmask',
+            '24',
+            '--device',
+            'etn0'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, InvalidArgumentException)
+
+    def test_set_ipv4_failure_no_netmask(self):
+        result = self.runner.invoke(set_ipv4, args=[
+            '--address',
+            '127.0.0.2',
+            '--netmask',
+            '--device',
+            'etn0'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, SystemExit)
+
+    def test_set_ipv4_failure_empty_netmask(self):
+        result = self.runner.invoke(set_ipv4, args=[
+            '--address',
+            '127.0.0.2',
+            '--netmask',
+            '',
+            '--device',
+            'etn0'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, InvalidArgumentException)
+
+    def test_set_ipv4_failure_no_device(self):
+        result = self.runner.invoke(set_ipv4, args=[
+            '--address',
+            '127.0.0.2',
+            '--netmask',
+            '24',
+            '--device'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, SystemExit)
+
+    def test_set_ipv4_failure_empty_device(self):
+        result = self.runner.invoke(set_ipv4, args=[
+            '--address',
+            '127.0.0.2',
+            '--netmask',
+            '24',
+            '--device',
+            ''
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, InvalidArgumentException)
 
     def test_set_mtu_success(self):
-        pass
+        result = self.runner.invoke(set_mtu, args=[
+            '--mtu',
+            '2200',
+            '--device',
+            'etn0'
+            ])
+        self.assertEqual(result.returncode, 0)
 
-    def test_set_mtu_failure(self):
-        pass
+    def test_set_mtu_failure_no_mtu(self):
+        result = self.runner.invoke(set_mtu, args=[
+            '--mtu',
+            '--device',
+            'etn0'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, SystemExit)
 
+    def test_set_mtu_failure_empty_mtu(self):
+        result = self.runner.invoke(set_mtu, args=[
+            '--mtu',
+            '',
+            '--device',
+            'etn0'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, InvalidArgumentException)
+
+    def test_set_mtu_failure_no_device(self):
+        result = self.runner.invoke(set_mtu, args=[
+            '--mtu',
+            '2020',
+            '--device'
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, SystemExit)
+
+    def test_set_mtu_failure_empty_device(self):
+        result = self.runner.invoke(set_mtu, args=[
+            '--mtu',
+            '2020',
+            '--device',
+            ''
+            ])
+        self.assertNotEqual(result.exit_code, 0)
+        self.assertIsInstance(result.exception, InvalidArgumentException)
 
 if __name__ == '__main__':
-    gw_test = GwCliTest()
-    all_members = inspect.getmembers(gw_test, inspect.ismethod)
-    for member in all_members:
-        if member[0].startswith('test_'):
-            member[1]()
+    unittest.main()
