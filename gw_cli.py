01c222575bb48fe41dde3947ceb405f3c8640d85
Andre Litty <andre.litty@gmx.de>
2020-03-29 13:54:04 +0700

    Fixed wrong naming in setup, adjusted entry_point for console script, added commands to command group

--- gw_cli.py
+++ gw_cli.py
@@ -4,7 +4,7 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-21 13:42:22
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-03-21 13:42:22
+# @Last Modified At: 2020-03-29 13:51:55
 # @Description: Command Line Tool to configure local network and dhcp settings on linux based machines.
 
 import click
@@ -51,7 +51,11 @@ def make_dhcp_server_config(begin_ip_range, end_ip_range, lease_time, domain_nam
     option domain {domain_name}
     """
 
-@click.command()
+@click.group()
+def cli():
+    click.echo('### GW-CLI ###')
+
+@cli.command()
 @click.option('--address', help='IPv4 address to assign to device')
 @click.option('--netmask', help='IPv4 netmask')
 @click.option('--device', help='Device to assign the address to')
@@ -61,7 +65,7 @@ def set_ipv4(address, netmask, device):
     args = ['ip', 'addr', 'add', address, netmask, 'dev', device]
     result = run_subprocess(args=args)
 
-@click.command()
+@cli.command()
 @click.option('--mtu', help='MTU to assign to device')
 @click.option('--device', help='Device to assign the MTU to')
 def set_mtu(mtu, device):
@@ -70,7 +74,7 @@ def set_mtu(mtu, device):
     args = ['ip', 'link', 'set', device, 'mtu', mtu]
     result = run_subprocess(args=args)
 
-@click.command()
+@cli.command()
 @click.option('--hostname', help='New hostname')
 def set_hostname(hostname):
     if len(hostname) == 0:
@@ -78,29 +82,19 @@ def set_hostname(hostname):
     args = ['hostnamectl', 'set-hostname', hostname]
     result = run_subprocess(args=args)
 
-@click.command()
+@cli.command()
 @click.option('--domainname', help='New domain name')
 @click.option('--beginIpRange', help='Begin of IP range')
 @click.option('--endIpRange', help='End of IP range')
 @click.option('--leaseTime', help='Lease time as string')
 def set_dhcp_server(domain_name, begin_ip_range, end_ip_range, lease_time):
-    if len(domainname) == 0
-    or len(begin_ip_range) == 0
-    or len(end_ip_range) == 0
-    of len(lease_time) == 0:
+    if len(domainname) == 0\
+    or len(begin_ip_range) == 0\
+    or len(end_ip_range) == 0\
+    or len(lease_time) == 0:
         raise InvalidArgumentException
     dhcp_server_config = make_dhcp_server_config(begin_ip_range, end_ip_range, lease_time, domain_name)
     with open('etc/udhcp.conf', 'w') as udhcp_conf:
         udhcp_conf.write(dhcp_server_config)
     args = ['udhcp', '/etc/udhcp.conf']
-    result = run_subprocess(args=args)
-
-# TODO: Get information how to handle client mac and ip address settings for dhcp
-@click.option('--clientMacAddress', help='MAC address of client')
-@click.option('--clientIPAddress', help='IP address of client')
-def set_dhcp_client(client_mac_address, client_ip_address):
-    if len(client_mac_address) == 0
-    or len(client_ip_address) == 0:
-        raise InvalidArgumentException
-    args = []
     result = run_subprocess(args=args)
