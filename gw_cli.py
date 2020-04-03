ad52b62233796e44a0b03d3553c12a3490369c5d
Andre Litty <andre.litty@gmx.de>
2020-04-03 11:31:51 +0700

    Separated code into commands and click cli to make import handling in other modules/packages easier

--- gw_cli.py
+++ gw_cli.py
@@ -4,60 +4,11 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-21 13:42:22
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-04-03 10:37:57
+# @Last Modified At: 2020-04-03 11:27:00
 # @Description: Command Line Tool to configure local network and dhcp settings on linux based machines.
 
 import click
-import subprocess
-
-
-class EmptyArgsException(Exception):
-    
-    def __init__(self, message='The passed arguments were empty'):
-        self.message = message
-    
-    def __str__(self):
-        return self.message
-
-class InvalidArgumentException(Exception):
-    
-    def __init__(self, message='The passed argument wasn\'t valid'):
-        self.message = message
-    
-    def __str__(self):
-        return self.message
-
-def run_subprocess(args=[]):
-    if not args or len(args) == 0:
-        raise EmptyArgsException()
-    try:
-        result = subprocess.run(
-            args,
-            capture_output=True,
-            check=True
-            )
-    except subprocess.CalledProcessError as cpe:
-        result = cpe
-    except Exception as e:
-        result = None
-    return result
-
-
-def make_dhcp_server_config(begin_ip_range, end_ip_range, lease_time, domain_name):
-    return f"""
-    start {begin_ip_range}
-    end {end_ip_range}
-    min_lease {lease_time}
-    option domain {domain_name}
-    """
-
-def get_dhcp_server_config():
-    if not os.path.isfile('etc/udhcp.conf'):
-        return []
-    with open('etc/udhcp.conf', 'r') as udhcp_conf:
-        config = udhcp_conf.readlines()
-    config = [line.strip() for line in config]
-    return [line.split(' ')[-1] for line in config]
+import commands
 
 @click.group()
 def cli():
@@ -68,31 +19,18 @@ def cli():
 @click.option('--netmask', help='IPv4 netmask')
 @click.option('--device', help='Device to assign the address to')
 def set_ipv4(address, netmask, device):
-    if not address\
-    or not netmask\
-    or not device:
-        raise InvalidArgumentException
-    args = ['ip', 'addr', 'add', address, netmask, 'dev', device]
-    result = run_subprocess(args=args)
+    commands.set_ipv4(address, netmask, device)
 
 @cli.command()
 @click.option('--mtu', help='MTU to assign to device')
 @click.option('--device', help='Device to assign the MTU to')
 def set_mtu(mtu, device):
-    if not mtu\
-    or  not device:
-        raise InvalidArgumentException
-    args = ['ip', 'link', 'set', device, 'mtu', mtu]
-    result = run_subprocess(args=args)
+    commands.set_mut(mtu, device)
 
 @cli.command()
 @click.option('--hostname', help='New hostname')
 def set_hostname(hostname):
-    if not hostname:
-        raise InvalidArgumentException
-    args = ['hostnamectl', 'set-hostname', hostname]
-    result = run_subprocess(args=args)
-    return result
+    commands.set_hostname(hostname)
 
 @cli.command()
 @click.option('--domain-name', help='New domain name')
@@ -100,14 +38,4 @@ def set_hostname(hostname):
 @click.option('--end-ip-range', help='End of IP range')
 @click.option('--lease-time', help='Lease time as string')
 def set_dhcp_server(domain_name, begin_ip_range, end_ip_range, lease_time):
-    if not domain_name\
-    or not begin_ip_range\
-    or not end_ip_range\
-    or not lease_time:
-        raise InvalidArgumentException
-    dhcp_server_config = make_dhcp_server_config(begin_ip_range, end_ip_range, lease_time, domain_name)
-    with open('etc/udhcp.conf', 'w') as udhcp_conf:
-        udhcp_conf.write(dhcp_server_config)
-    args = ['udhcp', '/etc/udhcp.conf']
-    result = run_subprocess(args=args)
-
+    commands.set_dhcp_server(domain_name, begin_ip_range, end_ip_range, lease_time)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-23 23:04:16
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-03-29 13:49:33
+# @Last Modified At: 2020-04-03 11:22:01
 # @Description: Setupfile to install gw-cli.
 
 from setuptools import setup
@@ -12,7 +12,7 @@ from setuptools import setup
 setup(
     name='gw-cli',
     version='0.1',
-    py_modules=['gw_cli'],
+    py_modules=['gw_cli', 'commands'],
     install_requires=[
         'Click'
     ],
