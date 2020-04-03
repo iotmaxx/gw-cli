04dff3b19d66ad04c60875058242a2e16c6af24b
Andre Litty <andre.litty@gmx.de>
2020-04-03 12:59:43 +0700

    Added meta information to setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-23 23:04:16
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-04-03 12:43:21
+# @Last Modified At: 2020-04-03 12:59:25
 # @Description: Setupfile to install gw-cli.
 
 from setuptools import setup
@@ -12,6 +12,10 @@ from setuptools import setup
 setup(
     name='gw-cli',
     version='0.1',
+    description='Gw-CLI to change network settings on Linux based systems',
+    author='Andre Litty',
+    author_email='alittysw@gmail.com',
+    url='https://github.com/iotmaxx/gw-cli',
     py_modules=['gw_cli'],
     install_requires=[
         'Click'
