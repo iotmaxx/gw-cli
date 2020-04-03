6f01cccb8a703505cb4c74549c213140b4ee10fe
Andre Litty <andre.litty@gmx.de>
2020-04-03 12:43:54 +0700

    Changed setup

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-23 23:04:16
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-04-03 12:37:51
+# @Last Modified At: 2020-04-03 12:43:21
 # @Description: Setupfile to install gw-cli.
 
 from setuptools import setup
 
 setup(
-    name='gw-commandline',
+    name='gw-cli',
     version='0.1',
     py_modules=['gw_cli'],
     install_requires=[
