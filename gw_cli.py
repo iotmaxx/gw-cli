ff5de07d9b4bde380375bc28c9b2eee20d326cd6
DerAndre <alittysw@gmail.com>
2020-08-06 16:49:03 +0200

    Implemented support for modem manager Implemented tests for modem manager

--- gw_cli.py
+++ gw_cli.py
@@ -4,8 +4,9 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-21 13:42:22
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-08-06 15:15:08
-# @Description: Command Line Tool to configure local network and dhcp settings on linux based machines.
+# @Last Modified At: 2020-08-06 16:45:58
+# @Description: Command Line Tool to configure local network and dhcp settings
+# on linux based machines.
 
 import click
 import subprocess
@@ -82,7 +83,8 @@ def run_subprocess(args=[]):
     return result
 
 
-def make_dhcp_server_config(begin_ip_range, end_ip_range, lease_time, domain_name):
+def make_dhcp_server_config(begin_ip_range, end_ip_range, lease_time,
+                            domain_name):
     return textwrap.dedent(f"""\
     start {begin_ip_range}
     end {end_ip_range}
@@ -123,7 +125,8 @@ def change_mtu(mtu, device='eth0'):
 
 def change_dhcp_server(domain_name, begin_ip_range, end_ip_range, lease_time):
     logger.info(
-        f'Setting new dhcp server config with {domain_name}, {begin_ip_range}, {end_ip_range}, {lease_time}')
+        f'Setting new dhcp server config with {domain_name}, {begin_ip_range},\
+        {end_ip_range}, {lease_time}')
     if not domain_name\
             or not begin_ip_range\
             or not end_ip_range\
@@ -176,6 +179,19 @@ def process_yaml(yml):
         return None
 
 
+def set_modem(con_name='mobile', operator_apn='internet'):
+    logger.info('Setting up modem')
+    args = ['nmcli', 'c', 'add', 'type', 'gsm', 'ifname', '"*"',
+            'con-name', con_name, 'apn', f'"{operator_apn}"']
+    setup_result = run_subprocess(args=args)
+    if setup_result.returncode == 0:
+        args = ['nmcli', 'c', 'up', con_name]
+        return run_subprocess(args=args)
+    else:
+        logger.error('Error while setting up modem')
+        return setup_result
+
+
 @click.group()
 def cli():
     click.echo('### GW-CLI ###')
@@ -183,8 +199,9 @@ def cli():
 
 @cli.command()
 @click.option('--address', help='IPv4 address to assign to device')
-@click.option('--netmask', help='IPv4 netmask supported formats: 99 (CIRD format) and 999.999.999.999 (long mask format)')
-@click.option('--device', default='eth0', help='Device to assign the address to')
+@click.option('--netmask', help='IPv4 netmask supported formats: 99\
+    (CIRD format) and 999.999.999.999 (long mask format)')
+@click.option('--device', default='eth0', help='Device to assign the address')
 def set_ipv4(address, netmask, device):
     change_ipv4(address, netmask, device)
 
@@ -235,3 +252,10 @@ def load_from_yaml(yml):
         dhcp_server.get('endIpRange'),
         dhcp_server.get('leaseTime')
     )
+
+
+@click.command()
+@click.option('--apn', default='internet', help='APN the modem is set to')
+@click.option('--name', default='mobile', help='Connection name')
+def setup_modem(apn, name):
+    set_modem(con_name=name, operator_apn=apn)
